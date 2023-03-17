01834fe304ed63a1dbbfaa53c6d6dfb991f88611
fernandoCV19 <fernandocuva@gmail.com>
2023-03-17 16:52:23 -0400

    Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import './App.css';
 import { PageProvider } from './pages/HomePageAdmin/context/PageContext';
 import { HomePageAdmin } from './pages/HomePageAdmin/HomePageAdmin';
+import { Login } from './pages/Login/Login';
 
 function App() {
-  return (
-    <PageProvider>
-      <HomePageAdmin />
-    </PageProvider>
-  );
+  // return (
+  //   <PageProvider>
+  //     <HomePageAdmin />
+  //   </PageProvider>
+  // ); 
+  return (<Login/>)
 }
 
 export default App;
